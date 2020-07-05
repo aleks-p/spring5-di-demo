b4c257c74da3e90d7c42e34b7ef7bf5b56439e8f
aleks-p <geniusap@gmail.com>
2020-07-05 16:37:07 +0200

    Add property configurer and an example for ready property files

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,33 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    private String username;
+
+    @Value("${guru.password}")
+    private String password;
+
+    @Value("${guru.dburl}")
+    private String dbUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        return new FakeDataSource(username, password, dbUrl);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        return configurer;
+    }
+}
